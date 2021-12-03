1fd21f82380c19d58313f19a72e65957fd654755
painteyes <andreantoniocchipinti@gmail.com>
2021-12-03 16:28:21 +0100

    big images data structure

--- js/script.js
+++ js/script.js
@@ -1,104 +1,48 @@
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
+Vue.config.devtools = true;
 
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-];
+// Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
 
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-];
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 
-// Popolo immagini e thumbnails dagli array
-const bigImagesContainer = document.querySelector('.big-images-container');
-const thumbsContainer = document.querySelector('.thumbs');
+const app = new Vue (
+    {
+        el: '#root',
+        data: {
+            currentActiveSlide: 0,
+            slides : [
+                {
+                    title: 'Svezia',
+                    image: 'img/01.jpg' ,
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    title: 'Svizzera',
+                    image: 'img/02.jpg' ,
+                    text: 'Lorem ipsum.'
+                },
+                {
+                    title: 'Gran Bretagna',
+                    image: 'img/03.jpg' ,
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+                },
+                {
+                    title: 'Germania',
+                    image: 'img/04.jpg' ,
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+                },
+                {
+                    title: 'Paradise',
+                    image: 'img/05.jpg' ,
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+                }
+            ], 
+        },
+        methods: {
 
-for( let i = 0; i < items.length; i++ ) {
-    // Quali variabili mi servono per popolare il mio nuovo div ad ogni ciclo?
-    // Path
-    const thisPath = items[i];
-    // Alt/Titolo
-    const thisTitle = title[i];
-    // Testo/Descrizione
-    const description = text[i];
-
-    // Popolare il container big images
-    const newDiv = `
-    <div class="single-image">
-        <img src="${thisPath}" alt="${thisTitle}">
-
-        <div class="single-image-text">
-            <h3>${thisTitle}</h3>
-            <p>${description}</p>
-        </div>
-    </div>
-    `;
-
-    bigImagesContainer.innerHTML += newDiv;
-
-    // Popolare thumbnails a destra
-    const newThumbnail = `
-    <div class="single-thumb">
-        <img src="${thisPath}" alt="${thisTitle}">
-    </div>
-    `;
-
-    thumbsContainer.innerHTML += newThumbnail;
-}
-
-// Definire l'elemento di default
-let activeItem = 0;
-const allBigImages = document.getElementsByClassName('single-image');
-const allThumbnails = document.getElementsByClassName('single-thumb');
-allBigImages[activeItem].classList.add('active');
-allThumbnails[activeItem].classList.add('active');
-
-// Eventi sulle arrows (con l'idea di spostare la classe active all'elemento precedente o successiv)
-const arrowDown = document.querySelector('.down');
-arrowDown.addEventListener('click', function() {
-    // Rimuovo la classe active alla big image e alla thumbnail corrente
-    allBigImages[activeItem].classList.remove('active');
-    allThumbnails[activeItem].classList.remove('active');
-
-    // Aumento activeImage di 1
-    if( activeItem < allBigImages.length - 1 ) {
-        activeItem++;
-    } else {
-        activeItem = 0;
-    }
-
-    // Aggiungo la classe active agli elementi big image e thumbnail che hanno il nuovo indice
-    allBigImages[activeItem].classList.add('active');
-    allThumbnails[activeItem].classList.add('active');
-});
-
-const arrowUp = document.querySelector('.up');
-arrowUp.addEventListener('click', function() {
-    // Rimuovo la classe active alla big image e alla thumbnail corrente
-    allBigImages[activeItem].classList.remove('active');
-    allThumbnails[activeItem].classList.remove('active');
-
-    // Decrementare activeImage di 1
-    if( activeItem > 0 ) {
-        activeItem--;
-    } else {
-        activeItem = allBigImages.length - 1;
-    }
-
-    // Aggiungo la classe active agli elementi big image e thumbnail che hanno il nuovo indice
-    allBigImages[activeItem].classList.add('active');
-    allThumbnails[activeItem].classList.add('active');
-});
+        }
+    } 
+     
+);
